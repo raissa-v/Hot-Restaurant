cac01afff7f168aad8508f449c6709bbaa46bb15
kyuu <arattanasack@gmail.com>
2020-01-06 21:04:00 -0800

    fixes routes

--- server.js
+++ server.js
@@ -6,41 +6,9 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 
-var customers = [
-    {
-        customerName: "Neil",
-        phoneNumber: "6506536000",
-        customerEmail: "neil.hammerle@marriott-sanmateo.com",
-        customerID: "Neil"
-    }
-];
- 
-  app.get("/api/customers/:customers", function(req, res) {
-    var chosen = req.params.customers;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < customers.length; i++) {
-      if (chosen === customers[i].customerName) {
-        return res.json(customers[i]);
-      }
-    }
-  
-    return res.json(false);
-  });
-  
+require('./routes/htmlRoutes')(app);
+require("./routes/apiRoutes")(app);
 
-  app.post("/api/customers", function(req, res) {
-    var newCustomer = req.body;
-  
-    newCustomer.customerName = newCustomer.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newCustomer);
-  
-    customers.push(newCustomer);
-  
-    res.json(newCustomer);
-  });
 
 app.listen(PORT, () => {
     console.log(`Currently listening on port: ${PORT}`);
